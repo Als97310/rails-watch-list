dffea8658431786dea27b6862a026a35fb66d06e
Andre Leroy <a.l.sismondino@outlook.fr>
2023-11-16 14:52:23 +0100

    ok

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -6,3 +6,6 @@
 // app/assets/config/manifest.js
 //= link popper.js
 //= link bootstrap.min.js
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'bookmarks/index'
+  get 'lists/index'
+  get 'lists/new'
+  get 'lists/create'
+  get 'lists/show'
+  get 'lists/edit'
+  get 'lists/update'
+  get 'lists/destroy'
+  get 'movies/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
